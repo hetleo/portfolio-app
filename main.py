5452123f366f21ba2140a8401c2ed4e17cf003f3
hetleo <hetleo@gmail.com>
2024-03-02 16:18:05 +0100

    Render app titles into two columns

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
-col1, col2 = st.columns(2)
+col1, col2 = st.columns([1,3])
+
+style = """
+<style>
+div[data-testid="st.Image"]{
+  float: right;
+}
+</style>
+"""
 
 with col1:
+    st.markdown(style, unsafe_allow_html=True)
     st.image("images/photo.png", width=150)
 
 with col2:
@@ -12,7 +22,7 @@ with col2:
     content = """
 Hei!
 
-Jeg heter Leon, og dette mitt lille hjørne av internett. Denne siden bruker jeg 
+Jeg er Leon, og dette mitt lille hjørne av internett. Denne siden bruker jeg 
 til å teste forskjellige programmeringsprosjekt. 
 
 Er du nysgjerrig? Ta en titt og bli med på eventyret!
@@ -20,7 +30,19 @@ Er du nysgjerrig? Ta en titt og bli med på eventyret!
     st.info(content)
 
 content2 = """
-Resten av denne siden er viet til å vise frem mine prosjekt. 
-Du kan trykke deg inn på de enkelte prosjektene for å lese mer og finne lenke til koden på Github.
+Resten av denne siden viser mine prosjekt. 
+Du kan trykke på Source Code for å komme til koden på Github.
 """
 st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+ 
+with col4:
+    for index, row in df[11:].iterrows():
+        st.header(row["title"])
