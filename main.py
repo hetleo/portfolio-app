62b66a1030a15b1d3cae5d89e780ac830373a923
hetleo <hetleo@gmail.com>
2024-03-02 12:02:17 +0100

    Add description on the new columns

--- main.py
+++ main.py
@@ -5,19 +5,22 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png", width=300)
+    st.image("images/photo.png", width=150)
 
 with col2:
     st.title("Leon Bang-Hetlevik")
     content = """
 Hei!
 
-Jeg heter Leon, og jeg er en bibliotekar med lidenskap for teknologi. 
-Jeg er en problemløser, en veileder og en nysgjerrig sjel som leter etter nye måter å bruke teknologi til å forbedre folks liv.
-Når jeg ikke er omgitt av bøker, liker jeg å utforske nye teknologier og leke meg med Python.
-
-På denne nettsiden kan du finne ut mer om mine prosjekter.
+Jeg heter Leon, og dette mitt lille hjørne av internett. Denne siden bruker jeg 
+til å teste forskjellige programmeringsprosjekt. 
 
 Er du nysgjerrig? Ta en titt og bli med på eventyret!
 """
     st.info(content)
+
+content2 = """
+Resten av denne siden er viet til å vise frem mine prosjekt. 
+Du kan trykke deg inn på de enkelte prosjektene for å lese mer og finne lenke til koden på Github.
+"""
+st.write(content2)
