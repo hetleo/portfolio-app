279136d5dba3f00caa67867357b443184808961a
hetleo <hetleo@gmail.com>
2024-03-03 21:48:57 +0100

    Added form with email sending

--- pages/Kontakt_meg.py
+++ pages/Kontakt_meg.py
@@ -1,3 +1,27 @@
-import streamlit as st 
+import streamlit as st
+import pandas
+from send_email import send_email
+
+
+df = pandas.read_csv("/Users/leon/prosjekt/portfolio-app/topics.csv")
+topics = df["Topics"].tolist()
+topics.insert(0, "Velg et tema...")
 
 st.header("Kontakt meg")
+
+with st.form(key="kontakt_skjema"):
+    user_email = st.text_input("Din epost-adresse")
+    topic = st.selectbox("Hva gjelder henvendelsen?", topics)
+    raw_message = st.text_area("Din melding her")
+    message = f"""\
+Subject: Ny epost - {topic}
+
+Fra: {user_email}
+
+{raw_message}
+"""
+    button = st.form_submit_button("Send melding")
+    if button:
+        send_email(message)
+        st.info("E-posten er sendt!")
+    
